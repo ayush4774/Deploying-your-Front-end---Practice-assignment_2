8d4c0f2833af48e4635e3569057899cfd4b1e69e
ayushhub-hub <ayushsable87@gmail.com>
2025-04-03 16:06:44 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div style={{ textAlign: "center", padding: "20px" }}>
+          <h1>AYUSH</h1>
+          <p>Contact: ayushsable@gmail.com</p>
+      </div>
+  );
+}
+
+export default App;
